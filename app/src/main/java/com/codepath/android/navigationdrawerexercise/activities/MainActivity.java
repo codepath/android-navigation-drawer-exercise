9237455502aaf15abfc09bbc98c17c454851c2ed
nidhi1608 <nidhi.1608@gmail.com>
2015-05-21 15:44:37 -0700

    added fragments

---
+++ app/src/main/java/com/codepath/android/navigationdrawerexercise/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.codepath.android.navigationdrawerexercise.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.codepath.android.navigationdrawerexercise.R;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
