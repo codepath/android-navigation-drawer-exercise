2e74005f65cf636e3cb63eaba76b2d2b6f45f1fb
JGeovani <geovani.santos74@gmail.com>
2015-10-15 20:28:14 -0300

    Review fragments

--- app/src/main/java/com/codepath/android/navigationdrawerexercise/fragments/SouthParkFragment.java
+++ app/src/main/java/com/codepath/android/navigationdrawerexercise/fragments/SouthParkFragment.java
@@ -9,12 +9,16 @@ import android.view.ViewGroup;
 
 import com.codepath.android.navigationdrawerexercise.R;
 
+
 public class SouthParkFragment extends Fragment {
 
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_south_park, container, false);
     }
+
+
+
 }
