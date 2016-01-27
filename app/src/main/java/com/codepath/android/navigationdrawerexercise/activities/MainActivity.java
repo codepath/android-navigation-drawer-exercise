fb2a4224bef85f282bf65e1136b1e365ad3ccd54
nidhi1608 <nidhi.1608@gmail.com>
2016-01-26 19:28:37 -0800

    updating to api 23

--- app/src/main/java/com/codepath/android/navigationdrawerexercise/activities/MainActivity.java
+++ app/src/main/java/com/codepath/android/navigationdrawerexercise/activities/MainActivity.java
@@ -1,12 +1,12 @@
 package com.codepath.android.navigationdrawerexercise.activities;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 
 import com.codepath.android.navigationdrawerexercise.R;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
