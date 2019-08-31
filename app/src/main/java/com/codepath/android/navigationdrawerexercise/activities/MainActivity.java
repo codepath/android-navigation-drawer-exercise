c1de15119fb4a5f30b0b0f6ae116d19d62dbabc6
Slava Panasenko <1201814+slavkoder@users.noreply.github.com>
2019-08-31 11:11:28 -0400

    Migrate to AndroidX (#8)
    * Migrate to AndroidX
    
    * Add build tools version
    
    * Update CI config to include build tools
    
    * Remove build-tools from build.gradle
    
    * Update build-tools to 28.0.3

--- app/src/main/java/com/codepath/android/navigationdrawerexercise/activities/MainActivity.java
+++ app/src/main/java/com/codepath/android/navigationdrawerexercise/activities/MainActivity.java
@@ -1,7 +1,7 @@
 package com.codepath.android.navigationdrawerexercise.activities;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.codepath.android.navigationdrawerexercise.R;
 
